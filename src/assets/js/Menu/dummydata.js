6d204eaa97e773f3b2271cbfa2fc400a30b44826
Steven <stevengrey45@gmail.com>
2018-10-11 11:07:35 +0200

    Nouvelle Intégration du Projet

---
+++ src/assets/js/Menu/dummydata.js
@@ -0,0 +1,34 @@
+var dummyData = {
+    "Informations": "",
+    "Planning Alimentaire": "",
+    "Suivi Vétérinaire": "",
+    "Animaux": "<button value='Nouveau'/>" +
+        "<table>" +
+        "<tr>" +
+        "<th>Nom</th>" +
+        "<th>Age</th>" +
+        "<th>Sexe</th>" +
+        "<th>Etat</th>" + /*En Bonne Santé | Malade | Décédé*/
+        "<th>Supprimer</th>" +
+        "</tr>" +
+        "<tr>" +
+        "<td>Test</td>" +
+        "<td>2 ans</td>" +
+        "<td>Femelle</td>" +
+        "<td>" +
+        "<select>" +
+        "<option>En Bonne Santé</option>" +
+        "<option>Malade</option>" +
+        "<option>Décédé</option>" +
+        "</select>" +
+        "</td>" +        
+        "<td><button value='Supprimer'/></td>" +
+        "</tr>" +
+        "</table>",
+    "Reproduction": "",
+    "Terrarium": "",
+    "Conseils": "",
+    "Vidéos": "",
+    "Localisation Vétérinaire": "",
+    "Contact": "",
+}
